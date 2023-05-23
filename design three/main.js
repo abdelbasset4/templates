4f45c0fc7a26d7557332d3559eefca90064e16e5
abdelbasset4 <benatmani1999@gmail.com>
2023-05-23 15:14:27 +0200

    design three

---
+++ design three/main.js
@@ -0,0 +1,40 @@
+  var typed = new Typed('#typed', {
+    stringsElement: '#typed-strings',
+    typeSpeed: 50,
+  });
+
+// gsap.to(".main-img",{
+//     scale: 1.15, // Scale the image to 2 times its original size
+//     repeat: -1, // Repeat the animation infinitely
+//     yoyo: true, // Reverse the animation on each repeat
+//     duration: 1, // Animation duration in seconds
+// })
+
+// gsap.to('.color-body',{
+//     duration: 2,
+//     width:"100%"
+// })
+// gsap.to('nav ul li:first-child',{
+//     duration: 3,
+//     scale:1,
+//     delay:1,
+//     ease: "bounce.out"
+// })
+// gsap.to('nav ul li:last-child',{
+//     duration: 3,
+//     scale:1,
+//     delay:1.9,
+//     ease: "bounce.out"
+// })
+// gsap.to('nav ul li:nth-child(2)',{
+//     duration: 3,
+//     scale:1,
+//     delay:1.3,
+//     ease: "bounce.out"
+// })
+// gsap.to('nav ul li:nth-child(3)',{
+//     duration: 3,
+//     scale:1,
+//     delay:1.6,
+//     ease: "power2.out"
+// })
